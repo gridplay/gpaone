7004ecce1d532082b432f4f7cf0c6d8a933d5f2f
Ven <chrisx84@live.ca>
2024-03-19 20:49:18 -0400

    Update Provider.php

--- Provider.php
+++ Provider.php
@@ -30,7 +30,7 @@ class Provider extends AbstractProvider {
     }
     protected function mapUserToObject(array $user) {
         return (new User())->setRaw($user)->map([
-            'id'    => $user['id'],
+            'id'   => $user['id'],
             'name' => $user['name'],
             'grid' => $user['grid'],
             'uuid' => $user['uuid']
